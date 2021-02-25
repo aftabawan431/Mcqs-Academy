593c4dbdf0df2685030146a14f7ac7a33ba19b70
aftab <aftabawan431@gmail.com>
2021-02-25 13:57:09 +0500

    first

---
+++ lib/Home_Screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        color: Colors.lightBlueAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Center(child: Text('Select Class',style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 40,color: Colors.white,
+            ),)),
+            SizedBox(height: 30,),
+            Card(
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.ac_unit,color: Colors.black,),
+                title: Text('8th Class ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.black54,),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Card(
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.ac_unit,color: Colors.black,),
+                title: Text('9th Class ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Card(
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.ac_unit,color: Colors.black,),
+                title: Text('10th Class ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'Home_Screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: HomeScreen(),
+      ),
+    );
+
+  }
+}
